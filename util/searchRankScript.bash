0e5f1011eac82ebfc92f160217436d2c8c8cb7b1
ankingcodes <ankush.bhardwaj0@gmail.com>
2020-06-26 18:03:38 +0530

    formatted search rank

--- util/searchRankScript.bash
+++ util/searchRankScript.bash
@@ -1,6 +1,7 @@
 # Clone the package to be published from latest commit
 package=$(git diff --name-only HEAD HEAD~1)
 end=".end"
+
 # check for TOML file
 if [[ $package != *".toml"* ]]; then
   echo "Not TOML FILE"
@@ -17,6 +18,7 @@ name="$(grep name "$f" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
 version="$(grep version "$f" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
 git clone $fixed newPackage
 cd newPackage
+
 # Perform a series of check on the package and award points
 mason=./Mason.toml
 testDir=./test/
@@ -39,22 +41,19 @@ fi
 [[ -d "$testDir" ]] && score=$((score+1)) && echo "test dir found"
 [[ -d "$exampleDir" ]] && score=$((score+1)) && echo "example dir found"
 [[ -d "$srcDir" ]] && score=$((score+1)) && echo "src dir found"
+
 # points for number of examples 
 countForExamples=`ls -l ./example/*.chpl 2>/dev/null | wc -l`
 score=$((score+countForExamples))
+
 # points for number of tests
 countForTests=`ls -l ./test/*.chpl 2>/dev/null | wc -l`
 score=$((score+countForTests))
 echo "$score"
+
 # append package score to TOML cache file
 cd ../../../
 echo "[$name.\"$version\"]" >> cache.toml
 echo "score = $score" >> cache.toml
 echo "Wrote $name=$score to cache.toml"
-# setup git and ready to push
-#git config --global user.name "ankingcodes"
-#git config --global user.email "ankush.bhardwaj0@gmail.com"
-#git add cache.toml
-#git commit -m "added $name to cache"
-# git push using chapel authorization to the repo
 
