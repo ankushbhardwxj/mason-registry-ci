788a671d1155c4ef60f031aedf987eb667e8b3c3
ankingcodes <ankush.bhardwaj0@gmail.com>
2020-06-27 00:12:34 +0530

    improved scripts

--- util/searchRankScript.bash
+++ util/searchRankScript.bash
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Clone the package to be published from latest commit
 package=$(git diff --name-only HEAD HEAD~1)
 end=".end"
@@ -8,16 +9,16 @@ if [[ $package != *".toml"* ]]; then
   exit 0
 fi
 path="$package$end"
-cd $(dirname $path)
+cd "$(dirname "$path")" || exit 1
 FILE=$package
 basename "$FILE"
-f="$(basename -- $FILE)"
+f="$(basename -- "$FILE")"
 source="$(grep source "$f"| cut -d= -f2)"
 fixed=$(echo "$source" | tr -d '"')
 name="$(grep name "$f" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
 version="$(grep version "$f" | cut -d= -f2 | tr -d '"' | tr -d ' ')"
-git clone $fixed newPackage
-cd newPackage
+git clone "$fixed" newPackage
+cd newPackage || exit 1
 
 # Perform a series of check on the package and award points
 mason=./Mason.toml
@@ -25,14 +26,14 @@ testDir=./test/
 exampleDir=./example/
 srcDir=./src/
 score=0
-countForREADME=`ls -l ./README.* 2>/dev/null | wc -l`
-if [ $countForREADME != 0 ]
+countForREADME=$(ls -l ./README.* 2>/dev/null | wc -l)
+if [ "$countForREADME" != 0 ]
 then
   echo "README found"
   score=$((score+1))
 fi
-countForModuleFile=`ls -l ./src/*.chpl 2>/dev/null | wc -l`
-if [ $countForModuleFile != 0 ]
+countForModuleFile=$(ls -l ./src/*.chpl 2>/dev/null | wc -l)
+if [ "$countForModuleFile" != 0 ]
 then
   echo "ModuleFile found"
   score=$((score+1))
@@ -43,16 +44,16 @@ fi
 [[ -d "$srcDir" ]] && score=$((score+1)) && echo "src dir found"
 
 # points for number of examples 
-countForExamples=`ls -l ./example/*.chpl 2>/dev/null | wc -l`
+countForExamples=$(ls -l ./example/*.chpl 2>/dev/null | wc -l)
 score=$((score+countForExamples))
 
 # points for number of tests
-countForTests=`ls -l ./test/*.chpl 2>/dev/null | wc -l`
+countForTests=$(ls -l ./test/*.chpl 2>/dev/null | wc -l)
 score=$((score+countForTests))
 echo "$score"
 
 # append package score to TOML cache file
-cd ../../../
+cd ../../../ || exit 1
 echo "[$name.\"$version\"]" >> cache.toml
 echo "score = $score" >> cache.toml
 echo "Wrote $name=$score to cache.toml"
